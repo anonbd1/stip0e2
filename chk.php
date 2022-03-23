5bd76efb083f51c5d2458ead66a745db5f454c79
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-03-24 00:36:13 +0600

    Update chk.php

--- chk.php
+++ chk.php
@@ -59,81 +59,43 @@ function mod($dividendo,$divisor)
 }
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/?nat=us');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_COOKIE, 1); 
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$resposta = curl_exec($ch);
-$firstname = value($resposta, '"first":"', '"');
-$lastname = value($resposta, '"last":"', '"');
-$phone = value($resposta, '"phone":"', '"');
-$zip = value($resposta, '"postcode":', ',');
-$state = value($resposta, '"state":"', '"');
-$email = value($resposta, '"email":"', '"');
-$city = value($resposta, '"city":"', '"');
-$street = value($resposta, '"street":"', '"');
-$numero1 = substr($phone, 1,3);
-$numero2 = substr($phone, 6,3);
-$numero3 = substr($phone, 10,4);
-$phone = $numero1.''.$numero2.''.$numero3;
-$serve_arr = array("gmail.com","homtail.com","yahoo.com.br","bol.com.br","yopmail.com","outlook.com");
-$serv_rnd = $serve_arr[array_rand($serve_arr)];
-$email= str_replace("example.com", $serv_rnd, $email);
-if($state=="Alabama"){ $state="AL";
-}else if($state=="alaska"){ $state="AK";
-}else if($state=="arizona"){ $state="AR";
-}else if($state=="california"){ $state="CA";
-}else if($state=="olorado"){ $state="CO";
-}else if($state=="connecticut"){ $state="CT";
-}else if($state=="delaware"){ $state="DE";
-}else if($state=="district of columbia"){ $state="DC";
-}else if($state=="florida"){ $state="FL";
-}else if($state=="georgia"){ $state="GA";
-}else if($state=="hawaii"){ $state="HI";
-}else if($state=="idaho"){ $state="ID";
-}else if($state=="illinois"){ $state="IL";
-}else if($state=="indiana"){ $state="IN";
-}else if($state=="iowa"){ $state="IA";
-}else if($state=="kansas"){ $state="KS";
-}else if($state=="kentucky"){ $state="KY";
-}else if($state=="louisiana"){ $state="LA";
-}else if($state=="maine"){ $state="ME";
-}else if($state=="maryland"){ $state="MD";
-}else if($state=="massachusetts"){ $state="MA";
-}else if($state=="michigan"){ $state="MI";
-}else if($state=="minnesota"){ $state="MN";
-}else if($state=="mississippi"){ $state="MS";
-}else if($state=="missouri"){ $state="MO";
-}else if($state=="montana"){ $state="MT";
-}else if($state=="nebraska"){ $state="NE";
-}else if($state=="nevada"){ $state="NV";
-}else if($state=="new hampshire"){ $state="NH";
-}else if($state=="new jersey"){ $state="NJ";
-}else if($state=="new mexico"){ $state="NM";
-}else if($state=="new york"){ $state="LA";
-}else if($state=="north carolina"){ $state="NC";
-}else if($state=="north dakota"){ $state="ND";
-}else if($state=="Ohio"){ $state="OH";
-}else if($state=="oklahoma"){ $state="OK";
-}else if($state=="oregon"){ $state="OR";
-}else if($state=="pennsylvania"){ $state="PA";
-}else if($state=="rhode Island"){ $state="RI";
-}else if($state=="south carolina"){ $state="SC";
-}else if($state=="south dakota"){ $state="SD";
-}else if($state=="tennessee"){ $state="TN";
-}else if($state=="texas"){ $state="TX";
-}else if($state=="utah"){ $state="UT";
-}else if($state=="vermont"){ $state="VT";
-}else if($state=="virginia"){ $state="VA";
-}else if($state=="washington"){ $state="WA";
-}else if($state=="west virginia"){ $state="WV";
-}else if($state=="wisconsin"){ $state="WI";
-}else if($state=="wyoming"){ $state="WY";
-}else{$state="KY";} 
+$name = ucfirst(str_shuffle('kurumi'));
+$last = ucfirst(str_shuffle('appisbest'));
 
+$first1 = str_shuffle("kurumi");
+$serve_arr = array("gmail.com","hotmail.com","yahoo.com.br","bol.com.br","yopmail.com","outlook.com");
+$serv_rnd = $serve_arr[array_rand($serve_arr)];
+$email = "".$first1."%40".$serv_rnd."";
+
+$street = "".rand(0000,9999)."+Main+Street";
+$ph = array("682","346","246");
+$ph1 = array_rand($ph);
+$phh = $ph[$ph1];
+$phone = "$phh".rand(0000000,9999999)."";
+
+$st = array("AL","NY","CA","FL","WA");
+$st1 = array_rand($st);
+$state = $st[$st1];
+if ($state == "NY"){
+$zip = "10080";
+$city = "New+York";
+}
+elseif ($state == "WA"){
+$zip = "98001";
+$city = "Auburn";
+}
+elseif ($state == "AL"){
+$zip = "35005";
+$city = "Adamsville";
+}
+elseif ($state == "FL"){
+$zip = "32003";
+$city = "Orange+Park";
+}
+else{
+$zip = "90201";
+$city = "Bell";
+};
 # -------------------- [1 REQ] -------------------#
 
 $ch = curl_init();
